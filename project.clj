48c56045f660336e1d70410228e12e4bf823cfb8
Florian Margaine <florian@margaine.com>
2012-12-30 15:15:16 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject my-website "0.1.0-SNAPSHOT"
+            :description "FIXME: write this!"
+            :dependencies [[org.clojure/clojure "1.4.0"]
+                           [org.clojure/math.numeric-tower "0.0.2"]
+                           [noir "1.3.0-beta3"]]
+            :main my-website.server)
---
+++ src/my_website/models/account.clj
@@ -0,0 +1,18 @@
+(ns my-website.models.account
+  (:require [clojure.math.numeric-tower :as math]))
+
+(defn calc-her-amount [my-salary her-salary total]
+  (math/round (/ (/ (* her-salary
+                       total)
+                    my-salary)
+                 (+ 1
+                    (/ her-salary
+                       my-salary)))))
+
+(defn calc-my-amount [total her-amount]
+  (math/round (- total her-amount)))
+
+(defn- calc [my-salary her-salary total]
+  (let [her-amount (calc-her-amount my-salary her-salary total)
+        my-amount (calc-my-amount total her-amount)]
+    ({:her-amount her-amount :my-amount my-amount})))
