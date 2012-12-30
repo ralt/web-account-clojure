1e6cf99f5ca12c874cd46085ebe1aa2131c2a7eb
Florian Margaine <florian@margaine.com>
2012-12-30 18:34:10 +0100

    Mixed private and public. Putting back the truth!

--- src/my_website/models/account.clj
+++ src/my_website/models/account.clj
@@ -1,7 +1,7 @@
 (ns my-website.models.account
   (:require [clojure.math.numeric-tower :as math]))
 
-(defn calc-her-amount [my-salary her-salary total]
+(defn- calc-her-amount [my-salary her-salary total]
   (math/round (/ (/ (* her-salary
                        total)
                     my-salary)
@@ -9,10 +9,10 @@
                     (/ her-salary
                        my-salary)))))
 
-(defn calc-my-amount [total her-amount]
+(defn- calc-my-amount [total her-amount]
   (math/round (- total her-amount)))
 
-(defn- calc [my-salary her-salary total]
+(defn calc [my-salary her-salary total]
   (let [her-amount (calc-her-amount my-salary her-salary total)
         my-amount (calc-my-amount total her-amount)]
     {:her-amount her-amount :my-amount my-amount}))
